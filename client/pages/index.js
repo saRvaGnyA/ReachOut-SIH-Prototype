e8cb2d1d30969a87a624434b6b8dc84f257db1f1
Ketaki Deshmukh <KetakiMDeshmukh>
2022-08-15 16:46:46 +0530

    basic login form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,46 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Login
+        </h2>
+
+        <form>
+          <div class="grid grid-cols-1 gap-6 mt-4">
+            <div>
+              <label
+                class="text-gray-700 dark:text-gray-200"
+                for="emailAddress"
+              >
+                Email Address
+              </label>
+              <input
+                id="emailAddress"
+                type="email"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              ></input>
+            </div>
+
+            <div>
+              <label class="text-gray-700 dark:text-gray-200" for="password">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              ></input>
+            </div>
+          </div>
+
+          <div class="flex justify-end mt-6">
+            <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
