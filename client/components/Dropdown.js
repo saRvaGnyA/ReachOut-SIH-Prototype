7ddf3fe39de5e808cbcaa89f429dbaf7eebe7184
Harsh <harshnag23@gmail.com>
2022-08-16 19:39:21 +0530

    Accessibility almost done

--- client/components/Dropdown.js
+++ client/components/Dropdown.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Dropdown() {
+function Dropdown(props) {
   const [openDropdown, setOpenDropdown] = useState(false);
 
   return (
@@ -19,9 +19,30 @@ function Dropdown() {
       </button>
       {openDropdown && (
         <>
-          <button className="material-icons floating-btn-1"></button>
-          <button className="material-icons floating-btn-2"></button>
-          <button className="material-icons floating-btn-3"></button>
+          <button
+            className="material-icons floating-btn-1"
+            onClick={() => {
+              props.fontInc();
+            }}
+          >
+            text_increase
+          </button>
+          <button
+            className="material-icons floating-btn-2"
+            onClick={() => {
+              props.fontDec();
+            }}
+          >
+            text_decrease
+          </button>
+          <button
+            className="material-icons floating-btn-3"
+            onClick={() => {
+              props.func();
+            }}
+          >
+            mode_edit
+          </button>
         </>
       )}
     </div>
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,14 +3,39 @@ import { ThemeProvider } from 'next-themes';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Dropdown from '../components/Dropdown';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [font, setFont] = useState(true);
+  const [fontSize, setFontSize] = useState(1);
+
+  function changeFont() {
+    setFont(!font);
+  }
+  function increaseFont() {
+    if (fontSize !== 5) {
+      setFontSize(fontSize + 1);
+    }
+    console.log(fontSize);
+  }
+  function decreaseFont() {
+    if (fontSize !== 1) {
+      setFontSize(fontSize - 1);
+    }
+    console.log(fontSize);
+  }
   return (
     <ThemeProvider attribute="class">
-      <Header />
-      <Component {...pageProps} />
-      <Dropdown/>
-      <Footer />
+      <div class={font ? 'font1' : 'font2'} id={`fontSize${fontSize}`}>
+        <Header />
+        <Component {...pageProps} />
+        <Dropdown
+          func={changeFont}
+          fontDec={decreaseFont}
+          fontInc={increaseFont}
+        />
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 }
