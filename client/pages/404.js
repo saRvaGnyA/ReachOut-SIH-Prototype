84337589b3f142ffba3cc7db4117713a61ea0c51
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-08-17 23:29:13 +0530

    404.js viewport width fix

--- client/pages/404.js
+++ client/pages/404.js
@@ -8,7 +8,7 @@ function error() {
 
   return (
     <div>
-      <div className="h-screen w-screen bg-gray-100 flex items-center">
+      <div className="h-screen bg-gray-100 flex items-center">
         <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
           <div className="max-w-md">
             <div className="text-5xl font-dark font-bold">404</div>
